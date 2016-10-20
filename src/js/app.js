d107f40553ce15f3f050e125ee06858dba046906
vinutha-singtel <vinutha@singtel.com>
2016-10-20 16:16:30 +0800

    temp

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,41 @@
-var Logger = require('./test');
+var request = require('superagent');
 
-// var logger = new Logger("debug", "my-app");
-// logger.log("error", "uksdsd", "111");
+const levels = {
+    error: 0, 
+    warn: 1, 
+    info: 2, 
+    verbose: 3, 
+    debug: 4, 
+    silly: 5
+}
 
-module.exports.Logger = Logger;
+class Logger {
+  constructor(logType, appName) {
+    this.logType = logType;
+    this.appName = appName;
+  }
+  
+  log = function(logType, msg, eventType, source, attributes, callback) {
+    console.log('Log this event' + this.appName);
+    console.log('default was ' + levels[this.logType]);    
+
+    if(levels[logType] <= levels[this.logType]) {
+    request
+        .post("http://localhost:8000/dslog")
+        .type('application/json')
+        .accept('json')
+        .send({
+          logType: logType,
+          msg: msg,
+          eventType: "UI_Event",
+          source: this.appName          
+        })
+        .end(function(err, res) {
+          if(err) 
+            throw err;
+        }.bind(this));
+    }
+  }
+}
+
+module.exports = Logger;
